10e69abbde789a9b72b9aa4514ae5d73c6feb58f
arin.oksas <arinoksas@outlook.com>
2022-05-21 01:02:38 +0300

    Day1 / Simple Types

---
+++ day1.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+var greeting = "Hello, playground"
+var population = 8_000_000 //integer
+var uzunYazi = """
+    Bu multi string tipinde bir text
+    """
+var uzunYazi2 = """
+    Bu multi text/
+    özzelliğinde bir/
+    yazı
+    """
+var pi = 3.141 // double
+var awesome = true //boolean
+var str = "Pi sayısı: \(pi)"
+var result = "Deneme \(str)"
+
+
+let taylor = "swift" //sabit tanımlama
+
+var album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
